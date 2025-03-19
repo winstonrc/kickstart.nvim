9571c5b67618ae470c11748fe84afb49c95d6743
Winston Cooke <winston@fieldguide.io>
2025-03-19 10:18:19 -0400

    select next/previous item in cmp with Tab

--- init.lua
+++ init.lua
@@ -220,7 +220,7 @@ vim.api.nvim_create_autocmd('VimEnter', {
       vim.cmd 'Neotree show'
       vim.defer_fn(function()
         vim.cmd 'Neotree focus'
-      end, 50)
+      end, 75)
     end
   end,
 })
@@ -817,10 +817,9 @@ require('lazy').setup({
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
         mapping = cmp.mapping.preset.insert {
           -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<Tab>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -828,7 +827,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<Tab>'] = cmp.mapping.confirm { select = true },
+          ['<Enter>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
